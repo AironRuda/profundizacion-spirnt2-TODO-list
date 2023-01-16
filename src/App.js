064e5448f9b90273ddaa21fec8b1288daf8bdbdc
AironRuda <airon.ruda@udea.edu.co>
2023-01-16 07:30:02 -0500

    todo list component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Title from "./components/Title";
 import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+import Todo from "./components/Todo";
 
 function App() {
   return (
-    <div className="h-full flex items-center justify py-20 px-5">
-      <div className="container flex flex-col max-w-xl">
+    // <div className="h-full flex items-center justify py-20 px-5">
+    <div className=" h-100 d-flex align-items-center justify-content-around pt-5">
+      <div className="container d-flex flex-column align-items-center justify-content-center">
         <Title />
         <TodoInput />
+        <TodoList>
+          <Todo />
+        </TodoList>
       </div>
     </div>
   );

e4705a31c399a9bd0528765c1f73d21d17faae72
AironRuda <airon.ruda@udea.edu.co>
2023-01-16 22:20:43 -0500

    feat: todo filters box

--- src/components/TodoFilters.component.jsx
+++ src/components/TodoFilters.component.jsx
@@ -16,5 +16,19 @@ const ItemsLeft = ({total = 0}) => {
   )
 }
 
+const FilterButtonContainer = ({children}) => {
+  return(
+    <div className="">
+      {children}
+    </div>)
+}
 
-export {FiltersContainer , ItemsLeft}
+const FilterButton = ({action, active, filter}) => {
+  return(
+    <button className={`hover:text-white cursor-pointer transitio-all duration-300 ease-in-out`
+    +(active.toLowerCase().includes(filter.toLowerCase())? "text-wite":"text-black")}>
+      {filter}
+    </button>)
+}
+
+export {FiltersContainer , ItemsLeft, FilterButtonContainer, FilterButton}
--- src/components/TodoFilters.js
+++ src/components/TodoFilters.js
@@ -1,10 +1,23 @@
 import React from "react";
-import { FiltersContainer, ItemsLeft } from "./TodoFilters.component";
+import {
+  FilterButton,
+  FilterButtonContainer,
+  FiltersContainer,
+  ItemsLeft,
+} from "./TodoFilters.component";
 
 const TodoFilters = () => {
   return (
     <FiltersContainer>
       <ItemsLeft />
+      <FilterButtonContainer>
+        <FilterButton action={() => {}} active="All" filter="All" />
+        <FilterButton action={() => {}} active="All" filter="Active" />
+        <FilterButton action={() => {}} active="All" filter="Completed" />
+      </FilterButtonContainer>
+      <button className=" text-gray-400 hove:text-white">
+        Clear Completed
+      </button>
     </FiltersContainer>
   );
 };
